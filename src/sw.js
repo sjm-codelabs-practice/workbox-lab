1d902634ffb9d6fefb76377a2fc1a56c898c0252
stevenjmarsh <sjm.atl@gmail.com>
2018-10-18 11:14:33 -0400

    adding route based cacheFirst for articles

--- src/sw.js
+++ src/sw.js
@@ -5,6 +5,19 @@ if (workbox) {
 
   workbox.precaching.precacheAndRoute([]);
 
+  workbox.routing.registerRoute(
+    /(.*)articles(.*)\.(?:png|gif|jpg)/,
+    workbox.strategies.cacheFirst({
+      cacheName: 'images-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        })
+      ]
+    })
+  );
+
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
